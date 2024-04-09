9c21c08545c4e24eca288e8d5e85fd77f0145367
liuzy0708 <liuzy21@mails.tsinghua.edu.cn>
2024-04-09 23:03:10 +0800

    update

--- OAL_strategies/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .random_sampling import RandomSampling
-# from .uncertainty_sampling import UncertaintySampling
-# from .up_to_labelratio_sampling import UptolabelratioSampling
-# from .chunk_random_sampling import ChunkRandomSampling
